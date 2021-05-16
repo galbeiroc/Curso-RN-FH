cf9f9dd0544395e79f9b3a98e21bda664aa2d8b6
galbeiroc <alcresgu_1989@hotmail.com>
2021-05-16 17:13:24 -0500

    Implement screenOptions tab navigator

--- 04-navigationApp/src/navigator/Tabs.tsx
+++ 04-navigationApp/src/navigator/Tabs.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Text } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import BottomTab1 from '../Tabs/BottomTab1';
@@ -20,10 +21,30 @@ function BottomTabs() {
           borderTopColor: colors.primary,
           borderTopWidth: 0,
           elevation: 0
-        }
+        },
+        labelStyle: {
+          fontSize: 15
+        },
       }}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, focused, size }) => {
+          let iconName: string = '';
+          switch(route.name) {
+            case 'BottomTab1':
+              iconName='tb1';
+            break;
+            case 'BottomTab2':
+              iconName='tb2';
+            break;
+            case 'StackNavigator':
+              iconName='stk';
+            break;
+          }
+          return <Text style={{ color }}>{iconName}</Text>
+        }
+      })}
     >
-      <Tab.Screen name="BottomTab1" options={{ title: 'Tab1'}} component={BottomTab1} />
+      <Tab.Screen name="BottomTab1" options={{ title: 'Tab1' }} component={BottomTab1} />
       <Tab.Screen name="BottomTab2" options={{ title: 'Tab2'}} component={BottomTab2} />
       <Tab.Screen name="StackNavigator" options={{ title: 'Stack'}} component={StackNavigator} />
     </Tab.Navigator>
@@ -36,4 +57,11 @@ export default BottomTabs;
 /**
  * tabBarOptions: => Personalizar los tabs
  * sceneContainerStyle:=> Personalizar content tabs
+ * screenOptions:=> Default options para todos screen return react element
+ * 
+ * <Tab.Screen
+ *  name="BottomTab1"
+ *  options={{ title: 'Tab1', tabBarIcon: (props) => <Text style={{ color: props.color }}>T1</Text>}}
+ *  component={BottomTab1}
+ * />
  */
