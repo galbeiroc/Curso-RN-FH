0314e22daef4f69748db59960915d0ab7f7f2a11
galbeiroc <alcresgu_1989@hotmail.com>
2021-05-16 17:45:56 -0500

    Install and implement material-bottom-tabs for android

--- 04-navigationApp/src/navigator/Tabs.tsx
+++ 04-navigationApp/src/navigator/Tabs.tsx
@@ -1,17 +1,55 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Platform, Text } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
 import BottomTab1 from '../Tabs/BottomTab1';
 import BottomTab2 from '../Tabs/BottomTab2';
 import StackNavigator from './StackNavigator';
 import { colors } from '../theme/appTheme';
 
-const Tab = createBottomTabNavigator();
+const TabIos = createBottomTabNavigator();
+const TabAndroid = createMaterialBottomTabNavigator();
 
 function BottomTabs() {
+  return Platform.OS === 'ios' ? <BottomTabsIos /> : <BottomTabsAndroid />;
+}
+
+function BottomTabsAndroid() {
   return(
-    <Tab.Navigator
+    <TabAndroid.Navigator
+      sceneAnimationEnabled
+      barStyle={{
+        backgroundColor: colors.primary,
+      }}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, focused }) => {
+          let iconName: string = '';
+          switch(route.name) {
+            case 'BottomTab1':
+              iconName='tb1';
+            break;
+            case 'BottomTab2':
+              iconName='tb2';
+            break;
+            case 'StackNavigator':
+              iconName='stk';
+            break;
+          }
+          return <Text style={{ color }}>{iconName}</Text>
+        }
+      })}
+    >
+      <TabAndroid.Screen name="BottomTab1" options={{ title: 'Tab1' }} component={BottomTab1} />
+      <TabAndroid.Screen name="BottomTab2" options={{ title: 'Tab2' }} component={BottomTab2} />
+      <TabAndroid.Screen name="StackNavigator" options={{ title: 'Stack' }} component={StackNavigator} />
+    </TabAndroid.Navigator>
+  )
+}
+
+function BottomTabsIos() {
+  return(
+    <TabIos.Navigator
       sceneContainerStyle={{
         backgroundColor: 'white'
       }}
@@ -44,10 +82,10 @@ function BottomTabs() {
         }
       })}
     >
-      <Tab.Screen name="BottomTab1" options={{ title: 'Tab1' }} component={BottomTab1} />
-      <Tab.Screen name="BottomTab2" options={{ title: 'Tab2'}} component={BottomTab2} />
-      <Tab.Screen name="StackNavigator" options={{ title: 'Stack'}} component={StackNavigator} />
-    </Tab.Navigator>
+      <TabIos.Screen name="BottomTab1" options={{ title: 'Tab1' }} component={BottomTab1} />
+      <TabIos.Screen name="BottomTab2" options={{ title: 'Tab2'}} component={BottomTab2} />
+      <TabIos.Screen name="StackNavigator" options={{ title: 'Stack'}} component={StackNavigator} />
+    </TabIos.Navigator>
   )
 };
 
@@ -58,6 +96,8 @@ export default BottomTabs;
  * tabBarOptions: => Personalizar los tabs
  * sceneContainerStyle:=> Personalizar content tabs
  * screenOptions:=> Default options para todos screen return react element
+ * sceneAnimationEnabled:=> Animations en el tab
+ * barStyle:=> Dar estilos al content tab
  * 
  * <Tab.Screen
  *  name="BottomTab1"
